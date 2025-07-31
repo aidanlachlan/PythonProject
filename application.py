df6819789311fd0f1bb2b182eb6fe78750764013
Aidan <aidanlmcmurray@gmail.com>
2025-07-30 17:43:18 -0700

    test

--- application.py
+++ application.py
@@ -1,16 +1,67 @@
-print("Hello World")
-print("This is the next statement")
+# Section 1
+# print("Hello World")
+# print("This is the next statement")
+#
+# number = 77
+# print(number)
+#
+# first_name = "Aidan"
+#
+# last_name = "Lachlan"
+#
+# name = first_name + " " + last_name
+# print(name)
+#
+# weight = 160
+# sentence = name + " weighs " + str(weight) + " lbs"
+# print(sentence)
 
-number = 77
-print(number)
+# Section 2
+# adult = True
+# data_type = type(adult)
+# age = 28
+# print(data_type)
+# print(age)
+# print(adult)
+#
+# adult = 12.08
+# data_type = type(adult)
+# print(data_type)
+#
+# adult = "String"
+# data_type = type(adult)
+# print(data_type)
 
-first_name = "Aidan"
+# Section 3
+# num1 = 3
+#
+# num2 = 10
+#
+# answer = num2 % num1
+# print(answer)
+# answer = 10 + 3 * 9 - 4
+#
+# print(answer)
 
-last_name = "Lachlan"
+# Section 4 // string indexing and slicing
+# sentence = 'This is a sentence'
+# # next_sentence = 'I'm coming home'
+# print(sentence)
+# # print(next_sentence)
+# next_sentence = "I'm coming home"
+# print(next_sentence)
+#
+# sentence = "I'm coming \nhome"
+# print(sentence)
+#
+# sentence = 'This is a sentence'
+# print(sentence[0])
+# print(sentence[-1])
+# print(sentence[0:4])
+# print(sentence[0:7])
+#
+# sentence = "abcdefg"
+# print(sentence[0:7:2])
+# print(sentence[3:])
+# print(sentence[-3:])
 
-name = first_name + " " + last_name
-print(name)
-
-weight = 160
-sentence = name + " weighs " + str(weight) + " lbs"
-print(sentence)
